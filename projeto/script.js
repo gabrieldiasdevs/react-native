c737131b1ba6e8d7813191879b0977d9de6a8bb3
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-03-21 20:30:48 -0300

    estudo matheus

---
+++ projeto/script.js
@@ -0,0 +1,9 @@
+var nome = prompt("Qual seu nome?")
+
+var sobrenome = prompt("Qual seu sobrenome?")
+
+console.log("Olá " + nome + " " + sobrenome)
+
+document.write("<h1> Bem vindo " + nome + " " + sobrenome + " </h1>")
+
+document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='Foto do Steve' />")
