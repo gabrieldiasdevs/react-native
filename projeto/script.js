28dcdd2a4e0dd888e26ea7606efe2a8ad662f67d
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-03-21 21:50:18 -0300

    functions

--- projeto/script.js
+++ projeto/script.js
@@ -22,3 +22,19 @@ function sair() {
     alert("Até mais!")
     area.innerHTML = "Você saiu!"
 }
+
+function mediaAluno(nota1, nota2) {
+    var media = (nota1 + nota2) / 2
+
+    if(media >= 7) {
+        console.log("Aluno aprovado com a média: " + media)
+    } else if(media < 7) {
+        console.log("Aluno reprovado com a média: " + media)
+    }
+}
+
+function aluno(nome, curso) {
+    var mensagem = "Seja bem vindo " + nome + " ao curso de " + curso
+    
+    console.log(mensagem)
+}
