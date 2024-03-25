0fcfddbbdfb98f4acb86dd0516add49e261545d2
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-03-25 16:09:34 -0300

    lista de tarefas finalizada

--- lista-de-tarefas/script.js
+++ lista-de-tarefas/script.js
@@ -1,16 +1,59 @@
-let listElement = document.querySelector('#app ul')
-let inputElement = document.querySelector('#app input')
-let buttonElement = document.querySelector('#app button')
+let listElement = document.querySelector("#app ul")
+let inputElement = document.querySelector("#app input")
+let buttonElement = document.querySelector("#app button")
 
-let tarefas = []
+let tarefas = JSON.parse(localStorage.getItem("@listaTarefas")) || []
+
+function renderTarefas() {
+    listElement.innerHTML = ""
+
+    tarefas.map((todo) => {
+        let liElement = document.createElement("li")
+        let tarefaText = document.createTextNode(todo)
+
+        let linkElement = document.createElement("a")
+        linkElement.setAttribute("href", "#")
+
+        let linkText = document.createTextNode(" Excluir")
+        linkElement.appendChild(linkText)
+
+        let posicao = tarefas.indexOf(todo)
+
+        linkElement.setAttribute("onclick", `deletarTarefa(${posicao})`)
+
+        liElement.appendChild(tarefaText)
+        liElement.appendChild(linkElement)
+        listElement.appendChild(liElement)
+
+    })
+}
+
+renderTarefas()
+
 
 function adicionarTarefas() {
-    if(inputElement.value === '') {
-        alert('Digite alguma tarefa!')
+    if(inputElement.value === "") {
+        alert("Digite alguma tarefa!")
         return false
     } else {
+        let novaTarefa = inputElement.value
 
+        tarefas.push(novaTarefa)
+        inputElement.value = ""
+
+        renderTarefas()
+        salvarDados()
     }
 }
 
 buttonElement.onclick = adicionarTarefas
+
+function deletarTarefa(posicao) {
+    tarefas.splice(posicao, 1)
+    renderTarefas()
+    salvarDados()
+}
+
+function salvarDados() {
+    localStorage.setItem("@listaTarefas", JSON.stringify(tarefas))
+}
