ae94947a9bb84954d1f610151f153be16d6718dc
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-03-25 14:43:11 -0300

    lista de tarefas

--- lista-de-tarefas/script.js
+++ lista-de-tarefas/script.js
@@ -1,6 +1,16 @@
-function registrar() {
-    let tarefa = document.getElementById('tarefa')
-    let criar = 
-        
-    
+let listElement = document.querySelector('#app ul')
+let inputElement = document.querySelector('#app input')
+let buttonElement = document.querySelector('#app button')
+
+let tarefas = []
+
+function adicionarTarefas() {
+    if(inputElement.value === '') {
+        alert('Digite alguma tarefa!')
+        return false
+    } else {
+
+    }
 }
+
+buttonElement.onclick = adicionarTarefas
