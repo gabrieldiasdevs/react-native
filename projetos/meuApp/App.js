d6de00785270555cb46cad92d03da064c0da2899
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-03-26 20:28:44 -0300

    criando app com react native

--- projetos/meuApp/App.js
+++ projetos/meuApp/App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, Text, Image } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+function App() {
+
+  let nome = 'Malu minha princesa'
+  let img = 'https://sujeitoprogramador.com/reactlogo.png'
+
+  return(
+    <View>
+      <Text>Olá Mundo!!!!</Text>
+      <Text style={{ color: '#FF0000', fontSize: 25, margin: 15 }}>
+        Meu primeiro App
+      </Text>
+      <Text style={{ fontSize: 30, color: 'green' }}>
+        Gabriel Dias
+      </Text>
+
+      <Image 
+        source={{ uri: img }}
+        style={{ width: 300, height: 300 }}
+      />
+
+      <Text style={{ fontSize: 40, color: 'red' }}>
+        {nome}
+      </Text>
+
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+
+function Logo() {
+  return(
+    <Image source={ uri: img } />
+  )
+}
