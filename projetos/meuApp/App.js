e5e7443dc5066c2aa20274711142f065d40fd47a
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-03-28 11:26:55 -0300

    continuidade no app biscoito

--- projetos/meuApp/App.js
+++ projetos/meuApp/App.js
@@ -1,16 +1,31 @@
-import React from 'react'
-import { View, Text, Image, Button, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 
 export default function App() {
+
+  const [img, setImg] = useState(require('./assets/biscoito.png'))
+
   return(
     <View style={styles.container}>
 
      <Image
-      style={styles.image}
-      source={'./assets/biscoito.png'}
-     />''
+      style={styles.img}
+      source={img}
+     />
 
-     <Button title='Quebrar Biscoito'/>
+     <Text style={styles.textoFrase}>" Essa é a primeira frase do bicoito! "</Text>
+
+     <TouchableOpacity style={styles.botao} onPress={ () => alert('TESTE') }>
+      <View style={styles.btnArea}>
+        <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
+      </View>
+     </TouchableOpacity>
+
+     <TouchableOpacity style={[styles.botao, { marginTop: 15, borderColor: '#121212' } ]} onPress={ () => alert('TESTE') }>
+      <View style={styles.btnArea}>
+        <Text style={[styles.btnTexto, { color: '#121212' } ]}>Quebrar Biscoito</Text>
+      </View>
+     </TouchableOpacity>
 
     </View>
   )
@@ -19,9 +34,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container:{
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  image:{
-    width: 400,
-    height: 400
+  img:{
+    width: 230,
+    height: 230,
+  },
+  textoFrase:{
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  botao:{
+    width: 230,
+    height: 50,
+    borderColor: '#dd7b22',
+    borderWidth: 2,
+    borderRadius: 25
+  },
+  btnArea:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnTexto:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#dd7b22'
   }
 })
