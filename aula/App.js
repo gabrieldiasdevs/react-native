0e4473ded22376b66d03984cc4959719736e8da1
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-03-31 23:56:02 -0300

    desafio feito

--- aula/App.js
+++ aula/App.js
@@ -1,25 +1,72 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Switch, TextInput, Button } from 'react-native';
+
 import Slider from '@react-native-community/slider'
+import {Picker} from '@react-native-picker/picker'
 
 export default function App() {
 
-  const [valor, setValor] = useState(50)
+  const[nome, setNome] = useState('')
+  const[idade, setIdade] = useState('')
+  const[sexo, setSexo] = useState(0)
+  const[limite, setLimite] = useState(1000)
+  const[estudante, setEstudante] = useState(false)
+
+  function alerta() {
+    alert(`Seu nome é: ${nome} e você tem: ${idade} anos! Você é: ${sexo ? 'Feminino' : 'Masculino'} e seu limite é de R$ ${limite.toFixed(0)},00 reais. Você ${estudante ? 'ainda' : 'não'} estuda.`)
+  }
 
   return (
     <View style={styles.container}>
       
-      <Slider
-      minimumValue={0}
-      maximumValue={100}
-      value={valor}
-      onValueChange={ (valorSelecionado) => setValor(valorSelecionado) }
-      minimumTrackTintColor='#000FFF'
-      maximumTrackTintColor='#FF0000'
-      thumbTintColor='#121212'
+      <TextInput
+      style={styles.input}
+      placeholder='Digite seu nome'
+      onChangeText={ (nomePessoa) => setNome(nomePessoa) }
       />
 
-      <Text style={styles.texto}>Você tem: {valor.toFixed(0)} kg </Text>
+      <TextInput
+      style={styles.input}
+      placeholder='Digite sua idade'
+      onChangeText={ (idadePessoa) => setIdade(idadePessoa) }
+      />
+
+      <Text style={styles.texto}>Selecione seu Sexo:</Text>
+
+      <Picker
+      style={styles.picker} 
+      selectedValue={sexo}
+      onValueChange={ (itemValue, itemIndex) => setSexo(itemValue) }
+      >
+        <Picker.Item key={0} value={0} label='Masculino' />
+        <Picker.Item key={1} value={1} label='Feminino' />
+      </Picker>
+
+      <Text style={styles.texto}>Selecione seu Limite: R$ {limite.toFixed(0)},00 </Text>
+
+      <Slider
+      style={styles.slider}
+      minimumValue={0}
+      maximumValue={3000}
+      value={limite}
+      onValueChange={ (limitePessoa) => setLimite(limitePessoa) }
+      thumbTintColor='#000000'
+      maximumTrackTintColor='gray'
+      minimumTrackTintColor='#00FF00'
+      />
+
+      <Text style={styles.texto}>Você é Estudante?</Text>
+
+      <Switch
+      style={styles.botao}
+      value={estudante}
+      onValueChange={ (estudaPessoa) => setEstudante(estudaPessoa) }
+      trackColor={{ false: '#FF0000', true: '#00FF00'}}
+      thumbColor={'#000000'}
+      />
+      
+      
+      <Button title='ABRIR CONTA' onPress={ alerta } />
 
     </View>
   );
@@ -28,11 +75,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 35,
+    marginTop: 45,
+    alignItems: 'center',
+  },
+  input:{
+    borderWidth: 1,
+    textAlign: 'center',
+    width: 300,
+    borderRadius: 15,
+    marginBottom: 10,
+  },
+  picker:{
+    width: 300,
+  },
+  slider:{
+    width: 300,
   },
   texto:{
-    textAlign: 'center',
-    fontSize: 25,
-    marginTop: 25,
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 20,
+  },
+  botao:{
+    marginBottom: 20,
   }
 });
