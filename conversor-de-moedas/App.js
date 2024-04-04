337aa56b3541b378d8692dfdf4f7bb5cee78ba35
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-04-04 20:33:19 -0300

    continuando app conversor de moedas

--- conversor-de-moedas/App.js
+++ conversor-de-moedas/App.js
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native'
+import { StyleSheet, Text, View, ActivityIndicator, TextInput, TouchableOpacity } from 'react-native'
 
 import { PickerItem } from './src/Picker'
 import { api } from './src/services/api'
 
 export default function App() {
 
-  const [moedas, setMoedas] = useState([])
   const [loading, setLoading] = useState(true)
+  const [moedas, setMoedas] = useState([])
+
+  const [moedaSelecionada, setMoedaSelecionada] = useState(null)
 
   useEffect(() => {
     async function loadMoedas() {
@@ -23,6 +25,7 @@ export default function App() {
       })
 
       setMoedas(arrayMoedas)
+      setMoedaSelecionada(arrayMoedas[0].key)
       setLoading(false)
 
     }
@@ -43,8 +46,24 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.areaMoeda}>
         <Text style={styles.titulo}>Selecione sua moeda</Text>
-        <PickerItem/>
+        <PickerItem
+          moedas={moedas}
+          moedaSelecionada={moedaSelecionada}
+          onChange={ (moeda) => setMoedaSelecionada(moeda) }
+        />
       </View>
+
+      <View style={styles.areaValor}>
+        <Text style={styles.titulo}>Digite um valor para converter em (R$)</Text>
+        <TextInput
+        keyboardType='numeric'
+          placeholder='EX: 1.50'
+          style={styles.input}
+        />
+      </View>
+      <TouchableOpacity style={styles.botaoArea}>
+        <Text style={styles.botaoText}>Converter</Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -62,6 +81,7 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 8,
     borderTopRightRadius: 8,
     padding: 8,
+    marginBottom: 1
   },
   titulo:{
     fontSize: 16,
@@ -69,5 +89,31 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     paddingLeft: 5,
     paddingTop: 5,
+  },
+  areaValor:{
+    width: '90%',
+    backgroundColor: '#f9f9f9',
+    paddingTop: 8,
+    paddingBottom: 8,
+  },
+  input:{
+    width: '100%',
+    padding: 8,
+    fontSize: 18,
+    color: '#000'
+  },
+  botaoArea:{
+    width: '90%',
+    backgroundColor: '#fb4b57',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+  },
+  botaoText:{
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 16
   }
 })
