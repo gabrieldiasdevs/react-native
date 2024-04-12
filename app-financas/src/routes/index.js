545b1f0d3fb61db18c93ac096cd6969176ae2d15
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-04-11 22:38:01 -0300

    terminado funcionalidade de permanecer usuário logado

--- app-financas/src/routes/index.js
+++ app-financas/src/routes/index.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { View, ActivityIndicator } from 'react-native'
 
 import { AuthContext } from '../contexts/auth'
 
@@ -6,9 +7,15 @@ import AuthRoutes from './auth.routes'
 import AppRoutes from './app.routes'
 
 export default function Routes(){
-  const loading = false
+  const { signed, loading } = useContext(AuthContext)
 
-  const { signed } = useContext(AuthContext)
+  if(loading){
+    return(
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#F0F4FF' }} >
+        <ActivityIndicator size='large' color='#131313' />
+      </View>
+    )
+  }
 
   return(
     signed ? <AppRoutes/> : <AuthRoutes/>
