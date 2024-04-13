3943578c1c7f279474f14459f9e8783be741587a
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-04-12 21:55:24 -0300

    usando date-fns na aplicação

--- app-financas/src/pages/Home/index.js
+++ app-financas/src/pages/Home/index.js
@@ -1,10 +1,27 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { View, Text, Button } from 'react-native'
 import { AuthContext } from '../../contexts/auth'
 import { Background } from './styles'
 import Header from '../../components/Header'
+import api from '../../services/api'
+import { format } from 'date-fns'
 
 export default function Home(){
+  const [listBalance, setListBalance] = useState([])
+
+  const [dateMovements, setDateMoviments] = useState(new Date())
+
+  useEffect(() => {
+    let isActive = true
+
+    async function getMovements(){
+      let dateFormated = format(dateMovements, 'dd/mm/yyyy')
+    }
+
+    getMovements()
+
+  }, [])
+
   return(
     <Background>
       <Header title='Minhas movimentações' />
