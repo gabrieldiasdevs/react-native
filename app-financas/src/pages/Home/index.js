7684f064c64a662ed98be1946f4f7658edcda26f
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-04-14 22:26:12 -0300

    receitas do app

--- app-financas/src/pages/Home/index.js
+++ app-financas/src/pages/Home/index.js
@@ -1,12 +1,18 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { View, Text, Button } from 'react-native'
 import { AuthContext } from '../../contexts/auth'
-import { Background } from './styles'
+import { 
+  Background, 
+  ListBalance } 
+from './styles'
 import Header from '../../components/Header'
 import api from '../../services/api'
 import { format } from 'date-fns'
+import { useIsFocused } from '@react-navigation/native'
+import BalanceItem from '../../components/BalanceItem'
 
 export default function Home(){
+  const isFocused = useIsFocused()
   const [listBalance, setListBalance] = useState([])
 
   const [dateMovements, setDateMoviments] = useState(new Date())
@@ -22,15 +28,30 @@ export default function Home(){
           date: dateFormated
         }
       })
+
+      if(isActive){
+        setListBalance(balance.data)
+      }
+
     }
 
     getMovements()
 
-  }, [])
+    return () => isActive = false
+
+  }, [isFocused])
 
   return(
     <Background>
       <Header title='Minhas movimentações' />
+
+      <ListBalance
+        data={listBalance}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={ item => item.tag }
+        renderItem={ ({ item }) => ( <BalanceItem data={item} /> ) }
+      />
     </Background>
   )
 }
--- app-financas/src/pages/Home/styles.js
+++ app-financas/src/pages/Home/styles.js
@@ -4,3 +4,7 @@ export const Background = styled.SafeAreaView`
   flex: 1;
   background-color: #F0F4FF;
 `
+
+export const ListBalance = styled.FlatList`
+  max-height: 190px;
+`
