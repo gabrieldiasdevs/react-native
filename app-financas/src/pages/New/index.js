fe709fc0ea47df1ec794822b67a937de36f7cd16
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-04-17 19:39:45 -0300

    página de registro finalizada

--- app-financas/src/pages/New/index.js
+++ app-financas/src/pages/New/index.js
@@ -2,12 +2,56 @@ import React, { useState } from 'react'
 import { Background, Input, SubmitButton, SubmitText } from './styles'
 import Header from '../../components/Header'
 import RegisterTypes from '../../components/RegisterTypes'
-import { SafeAreaView, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import { SafeAreaView, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native'
+import api from '../../services/api'
+import { format } from 'date-fns'
+import { useNavigation } from '@react-navigation/native'
 
 export default function New(){
   const [labelInput, setLabelInput] = useState('')
   const [valueInput, setValueInput] = useState('')
   const [type, setType] = useState('receita')
+
+  const navigation = useNavigation()
+
+  function handleSubmit(){
+    Keyboard.dismiss()
+    if(isNaN(parseFloat(valueInput)) || type === null){
+      alert('Preencha todos os campos')
+      return
+    }
+
+    Alert.alert(
+      'confirmando dados',
+      `Tipo: ${type} - Valor: ${parseFloat(valueInput)}`,
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel'
+        },
+        {
+          text: 'Continuar',
+          onPress: () => handleAdd()
+        }
+      ]
+    )
+  }
+
+  async function handleAdd(){
+    Keyboard.dismiss()
+
+    await api.post('/receive', {
+      description: labelInput,
+      value: Number(valueInput),
+      type: type,
+      date: format(new Date(), 'dd/MM/yyyy')
+    })
+
+    setLabelInput('')
+    setValueInput('')
+
+    navigation.navigate('Home')
+  }
   
   return(
     <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() } >
@@ -29,9 +73,10 @@ export default function New(){
 
           <RegisterTypes type={type} sendTypeChanged={ (item) => setType(item) } />
 
-          <SubmitButton>
+          <SubmitButton onPress={handleSubmit} >
             <SubmitText>Registrar</SubmitText>
           </SubmitButton>
+
         </SafeAreaView>
       </Background>
     </TouchableWithoutFeedback>
