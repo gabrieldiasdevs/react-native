f0d22949aa4a05db9d664744d9752ecbbe5b16d5
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-04-18 23:26:55 -0300

    página de perfil finalizada

--- app-financas/src/pages/Profile/index.js
+++ app-financas/src/pages/Profile/index.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import { Container, Message } from './styles'
+import React, { useContext } from 'react'
+import { 
+  Container, 
+  Message,
+  Name,
+  NewLink,
+  NewText,
+  LogoutButton,
+  LogoutText
+} from './styles'
 
 import Header from '../../components/Header'
+import { AuthContext } from '../../contexts/auth'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Profile(){
+  const { user, signOut } = useContext(AuthContext)
+  const navigation = useNavigation()
+
   return(
     <Container>
 
@@ -13,12 +26,16 @@ export default function Profile(){
       </Message>
 
       <Name numberOfLines={1} >
-        Gabriel
+        {user && user.name}
       </Name>
 
-      <NewLink>
+      <NewLink onPress={ () => navigation.navigate('Registrar') } >
         <NewText>Fazer registro</NewText>
       </NewLink>
+
+      <LogoutButton onPress={ () => signOut() } >
+        <LogoutText>Sair</LogoutText>
+      </LogoutButton>
 
     </Container>
   )
