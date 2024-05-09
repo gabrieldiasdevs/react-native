0581b82329fc4fbea2286b3ff7cbaf76c00b8091
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-05-09 09:21:49 -0300

    login e cadastro finalizado

--- heygrupos/src/pages/SignIn/index.js
+++ heygrupos/src/pages/SignIn/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { Platform } from 'react-native'
+import auth from '@react-native-firebase/auth'
+import { useNavigation } from '@react-navigation/native'
 
 import { 
   Container,
@@ -13,11 +15,47 @@ import {
 } from './styles'
 
 export default function SignIn(){
+  const navigation = useNavigation()
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-
   const [type, setType] = useState(true)
+
+  function handleLogin(){
+    if(type){
+
+      if(name === '' || email === '' || password === '') return
+
+      auth().createUserWithEmailAndPassword(email, password)
+      .then((user) => {
+        user.user.updateProfile({
+          displayName: name
+        })
+        .then(() => {
+          navigation.goBack()
+        })
+      })
+      .catch((error) => {
+        if(error.code === 'auth/email-already-in-use'){
+          console.log('Esse email já está em uso!')
+        }
+        if(error.code === 'auth/invalid-email'){
+          console.log('Email inválido!')
+        }
+      })
+
+    }else{
+      auth().signInWithEmailAndPassword(email, password)
+      .then(() => {
+        navigation.goBack()
+      })
+      .catch((error) => {
+        if(error.code === 'auth/invalid-email'){
+          console.log('Email inválido!')
+        }
+      })
+    }
+  }
 
   return(
     <Container>
@@ -45,9 +83,10 @@ export default function SignIn(){
         onChangeText={ (text) => setPassword(text) }
         placeholder='Sua senha'
         placeholderTextColor='#99999B'
+        secureTextEntry={true}
       />
 
-      <ButtonArea bg={ type ? "#F53745" : '#57DD86' }>
+      <ButtonArea bg={ type ? "#F53745" : '#57DD86' } onPress={handleLogin}>
         <ButtonText> {type ? 'Cadastrar' : 'Acessar'} </ButtonText>
       </ButtonArea>
 
