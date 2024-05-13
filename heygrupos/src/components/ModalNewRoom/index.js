fe096cee43ab78f61cd333bc8b28264e226e7c50
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-05-13 19:32:07 -0300

    criando as salas no firestore

--- heygrupos/src/components/ModalNewRoom/index.js
+++ heygrupos/src/components/ModalNewRoom/index.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import firestore from '@react-native-firebase/firestore'
+import auth from '@react-native-firebase/auth'
 
 import {
   Container,
@@ -13,6 +15,40 @@ import {
 export default function ModalNewRoom({ setVisible }){
   const [roomName, setRoomName] = useState('')
 
+  const user = auth().currentUser.toJSON()
+
+  function handleButtonCreate(){
+    if(roomName === '') return
+
+    createRoom()
+  }
+
+  function createRoom(){
+    firestore().collection('MESSAGE_THREADS')
+    .add({
+      name: roomName,
+      owner: user.uid,
+      lastMessage:{
+        text: `Grupo ${roomName} criado. Bem vindo(a)!`,
+        createdAt: firestore.FieldValue.serverTimestamp(),
+      }
+    })
+    .then((docRef) => {
+      docRef.collection('MESSAGES').add({
+        text: `Grupo ${roomName} criado. Bem vindo(a)!`,
+        createdAt: firestore.FieldValue.serverTimestamp(),
+        system: true,
+      })
+      .then(() => {
+        setVisible()
+      })
+
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
   return(
     <Container>
       <AreaOpacity activeOpacity={1} onPress={setVisible}></AreaOpacity>
@@ -24,7 +60,7 @@ export default function ModalNewRoom({ setVisible }){
           onChangeText={ (text) => setRoomName(text) }
           placeholder='Nome para sua sala?'
         />
-        <AreaButton>
+        <AreaButton onPress={handleButtonCreate}>
           <ButtonText> Criar sala </ButtonText>
         </AreaButton>
       </ModalContent>
