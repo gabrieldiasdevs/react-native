a9ddb94885d51b09a844daf2300f242d9f8b9793
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-05-12 21:39:37 -0300

    criando o componente do grupo da aplicação

---
+++ heygrupos/src/components/Room/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import {
+  Container,
+  Title,
+  Message
+} from './styles'
+
+export default function Room(){
+  return(
+    <Container>
+      <Title> Grupo 1 </Title>
+      <Message> Esse grupo é top! </Message>
+    </Container>
+  )
+}
---
+++ heygrupos/src/components/Room/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.TouchableOpacity`
+
+` 
+
+export const Title = styled.Text`
+
+` 
+
+export const Message = styled.Text`
+
+` 
