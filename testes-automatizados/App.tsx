1eb46bca7d3715fcd1acfa434449d9d681817139
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-06-04 19:06:42 -0300

    fazendo testes na aplicação

--- testes-automatizados/App.tsx
+++ testes-automatizados/App.tsx
@@ -1,9 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
 export default function App() {
+  const [counter, setCounter] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>App Contador</Text>
+      <Text style={styles.title}>App Contador</Text>
+
+      <View style={styles.counterArea}>
+        <TouchableOpacity onPress={() => setCounter(counter - 1)}>
+          <Text>-</Text>
+        </TouchableOpacity>
+
+        <Text testID='counter'>{counter}</Text>
+
+        <TouchableOpacity onPress={() => setCounter(counter + 1)}>
+          <Text>+</Text>
+        </TouchableOpacity>
+      </View>
+    
     </View>
   )
 }
@@ -15,4 +31,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    fontSize: 24,
+    marginTop: 14,
+    fontWeight: 'bold'
+  },
+  counterArea:{
+    flexDirection: 'row',
+    gap: 14,
+    marginTop: 14,
+    marginBottom: 14
+  }
 })
--- testes-automatizados/__test__/app/App.test.tsx
+++ testes-automatizados/__test__/app/App.test.tsx
@@ -1,12 +1,33 @@
-import { render } from '@testing-library/react-native'
+import { render, fireEvent } from '@testing-library/react-native'
 import App from '../../App'
 
-it("Test exemplo", () => {
+it('Test exemplo', () => {
 
-  expect(1).toBe(1)
+  const num1 = 10
+  const num2 = 20
+  const soma = num1 + num2
+
+  expect(soma).toBe(30)
 
 })
 
-it("Test render App component", () => {
-  render(<App/>)
+describe('App Component Test', () => {
+
+  it('should Test render App component', () => {
+    const { getByText } = render(<App/>)
+
+    expect(getByText('App Contador')).toBeTruthy()
+  })
+
+  it('should change counter on press button', () => {
+    const { getByText, getByTestId } = render(<App/>)
+
+    const button = getByText('+')
+    fireEvent.press(button)
+
+    const counterText = getByTestId('counter')
+
+    expect(counterText.props.children).toBe('')
+  })
+
 })
