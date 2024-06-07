e9585c3333e2103c99e445988a3b197b8449317f
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-06-06 21:13:48 -0300

    aprendendo utilizar o fakeTimers do jest

--- testes-automatizados/__test__/message/message.test.tsx
+++ testes-automatizados/__test__/message/message.test.tsx
@@ -1,7 +1,11 @@
-import { render, screen, fireEvent } from '@testing-library/react-native'
+import { render, screen, fireEvent, act } from '@testing-library/react-native'
 import { Message } from '../../src/Message'
 
 describe('Component Message', () => {
+
+  afterAll(() => {
+    jest.clearAllTimers()
+  })
   
   it('should render text message', () => {
     render(<Message/>)
@@ -11,11 +15,15 @@ describe('Component Message', () => {
   })
 
   it('should change message on click button', async () => {
+    jest.useFakeTimers()
     render(<Message/>)
 
     expect(screen.getByTestId('message').props.children).toBe('Aguardando...')
 
-    fireEvent.press(screen.getByText('Acessar'))
+    act(() => {
+      jest.runAllTimers()
+      fireEvent.press(screen.getByText('Acessar'))
+    })
 
     //expect(screen.getByTestId('message').props.children).toBe('Bem vindo Gabriel')
 
