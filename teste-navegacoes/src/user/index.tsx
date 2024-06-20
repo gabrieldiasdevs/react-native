94292f1af236810b240216dd78a961aa62fb72c8
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-06-19 21:35:34 -0300

    revisando módulo de testes automatizados

--- teste-navegacoes/src/user/index.tsx
+++ teste-navegacoes/src/user/index.tsx
@@ -13,6 +13,11 @@ type UserRouteProps = RouteProp<RouteUserParams, 'user'>
 export function User(){
   const route = useRoute<UserRouteProps>()
 
+  function handleCalculate(n1: number, n2: number){
+    const soma = n1 + n2
+    return soma
+  }
+
   return(
     <View style={styles.container}>
       <Text>Página Usuário</Text>
